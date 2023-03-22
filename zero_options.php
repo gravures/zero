044f71122e617f952faadae3b411897d0b2fa45a
Gilles Coissac <gilles@ideographe.fr>
2023-03-22 21:34:14 +0100

    commente code provisoirement

--- zero_options.php
+++ zero_options.php
@@ -5,14 +5,14 @@ $GLOBALS['z_blocs'] = array('content', 'aside', 'extra', 'head', 'head_js', 'hea
 
 define('_ALBUMS_INSERT_HEAD_CSS', false);
 
-if (
-	defined('_ZERO_AUTH_DEMO')
-	? _ZERO_AUTH_DEMO
-	: (
-		isset($GLOBALS['visiteur_session']['statut'])
-		and $GLOBALS['visiteur_session']['statut'] === '0minirezo'
-		and $GLOBALS['visiteur_session']['webmestre'] === 'oui'
-	)
-) {
-	_chemin(_DIR_PLUGIN_ZERO_DIST . "demo/");
-}
+// if (
+// 	defined('_ZERO_AUTH_DEMO')
+// 	? _ZERO_AUTH_DEMO
+// 	: (
+// 		isset($GLOBALS['visiteur_session']['statut'])
+// 		and $GLOBALS['visiteur_session']['statut'] === '0minirezo'
+// 		and $GLOBALS['visiteur_session']['webmestre'] === 'oui'
+// 	)
+// ) {
+// 	_chemin(_DIR_PLUGIN_ZERO_DIST . "demo/");
+// }
