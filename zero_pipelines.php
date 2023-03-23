32255da102866261956cc820e7f16da58941b638
Gilles Coissac <gilles@ideographe.fr>
2023-03-23 09:48:38 +0100

    Utilise le pipline insert_head pour l'integration de CssGridViewer.js

--- zero_pipelines.php
+++ zero_pipelines.php
@@ -7,27 +7,22 @@
  
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-function zero_affichage_final($flux) {
+
+function zero_insert_head($flux) {
 	if (
 		$GLOBALS['html']
 		and isset($GLOBALS['visiteur_session']['statut'])
 		and $GLOBALS['visiteur_session']['statut'] == '0minirezo'
 		and $GLOBALS['visiteur_session']['webmestre'] == 'oui'
-		and strpos($flux, '<!-- insert_head -->') !== false
-		and $p = stripos($flux, '</body>')
 	) {
-		if ($f = find_in_path('js/grid-viewer.js')) {
-			$flux = substr_replace($flux, '<script type="text/javascript" src="' . $f . '"></script>', $p, 0);
-		}
 		if (
-			(_VAR_MODE === 'debug' || _request('var_profile'))
-			and $p = stripos($flux, '</head>')
+			($cgv = find_in_path('js/cssGridViewer/css-grid-viewer.js'))
+			and ($gv = find_in_path('js/grid-viewer.js'))
 		) {
-			$file_css = direction_css(scss_select_css('css/spip.admin.css'));
-			$css = file_get_contents($file_css);
-			$css = "<style type='text/css'>$css</style>";
-			$flux = substr_replace($flux, $css, $p, 0);
+			$flux .= '<script type="text/javascript" src="' . $cgv . '"></script>'
+			         . '<script type="text/javascript" src="' . $gv . '"></script>';
 		}
 	}
 	return $flux;
 }
+
